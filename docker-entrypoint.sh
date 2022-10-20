d7ddb781fa102e4b5b84c2ec334573ef15c0493f
esme518 <esme518@gmail.com>
2022-10-20 23:23:19 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ ! -f app.py ]; then
-	git clone https://gitcode.net/qq_32394351/dr_py.git .
+	git clone -q https://gitcode.net/qq_32394351/dr_py.git .
 	rm -rf base/rules.db
 	echo "App Initialized"
-elif [ ! -z "$AUTOUPDATE" ]; then
+elif [ -n "$AUTOUPDATE" ]; then
 	mv base/rules.db /tmp
 	ls -A1 | xargs rm -rf
-	git clone https://gitcode.net/qq_32394351/dr_py.git .
+	git clone -q https://gitcode.net/qq_32394351/dr_py.git .
 	mv -f /tmp/rules.db base/rules.db
 	echo "App Updated"
 fi
