3d778cf2093c6dbb3967c385a827bbcc4c1b8cbc
esme518 <esme518@gmail.com>
2022-11-06 17:17:30 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,14 @@ if [ ! -f app.py ]; then
 	git clone -q ${REPO_URL} .
 	rm -rf base/rules.db
 	echo "App Initialized"
+	echo "Version $(cat js/version.txt)"
 elif [ -n "$AUTOUPDATE" ] && [ "$AUTOUPDATE" != 0 ]; then
 	mv base/rules.db base/直播.txt /tmp
 	ls -A1 | xargs rm -rf
 	git clone -q ${REPO_URL} .
 	mv -f /tmp/rules.db /tmp/直播.txt base/
 	echo "App Updated"
+	echo "Version $(cat js/version.txt)"
 fi
 
 if [ ! -f /etc/supervisord.conf ]; then
